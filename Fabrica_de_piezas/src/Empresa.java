c6653fbe5fe17a17a5b732a22b61aad10eac6a23
Andres Velez Zapata <andresvelez11@gmail.com>
2019-02-13 19:34:29 -0500

    Test

--- Fabrica_de_piezas/src/Empresa.java
+++ Fabrica_de_piezas/src/Empresa.java
@@ -4,10 +4,12 @@ public class Empresa {
 	private String nombre;
 	private String direcccion;
 	private Cliente clientes[] = new Cliente[0];
-	
-	public static void main(String[] args) {
-		
 
+	public static void main(String[] args) {
+
+		int a;
+		int b; 
+		int c;
 	}
 
 }
