ae762c5e10218ace6480e5907125283dec77ccdb
Andres Velez Zapata <andresvelez11@gmail.com>
2019-02-15 09:33:47 -0500

    Alzate pendejo

--- Fabrica_de_piezas/src/Empresa.java
+++ Fabrica_de_piezas/src/Empresa.java
@@ -4,6 +4,34 @@ public class Empresa {
 	private String nombre;
 	private String direcccion;
 	private Cliente clientes[] = new Cliente[0];
+	
+	public void creaeCliente() {
+		
+	}
+	
+	public void quitarCliente() {
+		
+	}
+	
+	public void hacerSolicitud() {
+		
+	}
+	
+	public void quitarSolicitud() {
+		
+	}
+	
+	public void buscarSolicitud() {
+		
+	}
+	
+	public void buscarCliente() {
+		
+	}
+	
+	public void vip() {
+		
+	}
 
 	public static void main(String[] args) {
 
